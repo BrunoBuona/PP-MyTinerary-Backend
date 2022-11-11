bba69726bec84d327c9ce51a34b39f3496da74c2
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-11-11 17:26:04 -0300

    MT-02-22

---
+++ models/data/hotels.js
@@ -0,0 +1,101 @@
+const hotels = [
+    {
+        name: 'Urban Garden Hotel',
+        photo: ['https://cf.bstatic.com/xdata/images/hotel/max1024x768/97155325.jpg?k=0601245d65a84eb01943d4e1f9de0f3b236ba6e53084a65403f602579633c1a3&o=&hp=1','https://images.trvl-media.com/lodging/19000000/18290000/18283200/18283164/81002268.jpg?impolicy=resizecrop&rw=670&ra=fit' , 'https://media-cdn.tripadvisor.com/media/photo-s/1b/45/34/da/exterior.jpg'],
+        capacity: 200,
+        citiId: 'Roma',
+        userId: '636e884578fa70e8f8c471f8'
+    },
+    {
+        name: 'MEININGER Hotel Paris Porte de Vincennes',
+        photo: ['https://media.staticontent.com/media/pictures/cc82e186-4caa-4b11-9540-9c832fb9a9d7', 'https://www.meininger-hotels.com/fileadmin/_processed_/d/e/csm_MEININGER-Hotel-Paris-Porte-de-Vincennes-Lounge-1-CSW5181-Print_625bdb5544.jpg' , 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/19/5f/68/meininger-hotel-paris.jpg?w=700&h=-1&s=1'],
+        capacity: 996,
+        citiId: 'Paris',
+        userId: '636e884578fa70e8f8c471f7'
+    },
+    {
+        name: 'Ibis Hotel',
+        photo: ['https://media.staticontent.com/media/pictures/456a3559-5e26-4df7-b656-94861f510d8f', 'https://www.ahstatic.com/photos/6502_rodbc_00_p_1024x768.jpg', 'https://burritadeviaje.com/wp-content/uploads/2019/09/hotel-ibis-obelisco.jpg'],
+        capacity: 470,
+        citiId: 'Buenos Aires',
+        userId: '636e884578fa70e8f8c471f7'
+    },
+    {
+        name: 'Barcelona Majestic Hotel',
+        photo: ['https://media-cdn.tripadvisor.com/media/photo-s/24/9c/90/f4/facade.jpg', 'https://cdn.kiwicollection.com/media/property/PR004294/xl/004294-La%20Dolce%20Vitae%20rooftop%20terrace%20majestic.jpg?cb=1655762639' , 'https://www.kayak.com.ar/rimg/himg/15/84/08/leonardo-1954148-Executive_room_majestic_hotel_O-830450.jpg?width=720&height=576&crop=true'],
+        capacity: 730,
+        citiId: 'Barcelona',
+        userId: '636e884578fa70e8f8c471f7'
+    },
+    {
+        name: 'Sultanahmet Hotel Han',
+        photo: ['https://media-cdn.tripadvisor.com/media/photo-s/12/03/42/a9/otel-han.jpg','https://pix10.agoda.net/hotelImages/233426/-1/a8a2f7157836ea269929a93920986faa.jpg?ca=10&ce=1&s=1024x768' , 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/136240900.jpg?k=dd949dc56933f5ea331f73529a5db094f54ea34af71d3371c78f3b519173234d&o=&hp=1'],
+        capacity: 140,
+        citiId: 'Estambul',
+        userId: '636e884578fa70e8f8c471f8'
+    },
+    {
+        name: 'Hyde Park Hotel',
+        photo: ['https://www.ngenespanol.com/wp-content/uploads/2018/08/Hospédate-en-el-único-hotel-en-Hyde-Park-en-Londres.jpg', 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/123348684.jpg?k=557a7a9a07e63fc559120de050cc34813122067773d3425d19d50061f43cf462&o=&hp=1' , 'https://y.cdrst.com/foto/hotel-sf/141cd/granderesp/grand-royale-london-hyde-park-zonasnobles-c85ea3c.jpg'],
+        capacity: 698,
+        citiId: 'Londres',
+        userId: '636e884578fa70e8f8c471f8'
+    },
+    {
+        name: 'Hotel Manhattan',
+        photo: ['https://cf.bstatic.com/xdata/images/hotel/max1280x900/20455439.jpg?k=bd4340dd5bc576ae6771c20f5bdf4f003f6abdf01da496c69c08e29e3edbb4bf&o=&hp=1', 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/40322044.jpg?k=81f63b562cc70a84c9855f6700841e9f1fe7abb1142336018484b3cfec28b9f8&o=&hp=1' , 'https://content.r9cdn.net/rimg/himg/34/02/2e/revato-37678-12480052-712271.jpg?width=500&height=350&xhint=1620&yhint=1000&crop=true'],
+        capacity: 51000,
+        citiId: 'Nueva York',
+        userId: '636e884578fa70e8f8c471f8'
+    },
+    {
+        name: 'Hotel Nuevo Madrid',
+        photo: ['https://www.riventi.net/wp-content/uploads/2016/01/hotel-nuevo-madrid-fachada-muro-cortina-Riventi-1.jpg', 'https://imgcy.trivago.com/c_fill,d_dummy.jpeg,e_sharpen:60,f_auto,h_627,q_auto,w_1200/itemimages/80/90/80902_v80905.jpeg', 'https://res.cloudinary.com/lastminute/image/upload/c_scale,w_630/q_auto/v1532849201/blzbqco5cdd5d7fxqwgo.jpg'],
+        capacity: 280,
+        citiId: 'Madrid',
+        userId: '636e884578fa70e8f8c471f5'
+    },
+    {
+        name: 'Hotel NH Gran Hotel Provincial',
+        photo: ['https://img.nh-hotels.net/vJEy/1ZZdy/original/F_NH_gran-hotel-provincial_018.jpg', 'https://img.nh-hotels.net/vJEy/DooLX/original/RSPV2_NH_gran-hotel-provincial_021.jpg?output-quality=80&resize=1600:*&background-color=white' , 'https://img.nh-hotels.net/vJEy/qNNLR/original/SP_NH_gran-hotel-provincial_009.jpg'],
+        capacity: 1670,
+        citiId: 'Amsterdam',
+        userId: '636e884578fa70e8f8c471f5'
+    },
+    {
+        name: 'Sheraton Mar Del Plata Hotel ',
+        photo: ['https://cf.bstatic.com/xdata/images/hotel/max1024x768/335658011.jpg?k=7744bd54f3b52559e191dc29bb12a8246f97ba9f6c3a822c064515b148cfb376&o=&hp=1','https://static11.com-hotel.com/uploads/hotel/14875/photo/sheraton-mar-del-plata-hotel_155198843031.jpg', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/3b/ce/6f/cruz-del-sur-meeting.jpg?w=700&h=-1&s=1'],
+        capacity: 1300,
+        citiId: 'Mar Del Plata',
+        userId: '636e884578fa70e8f8c471f5'
+    },
+    {
+        name: 'Imperial Hotel Tokyo',
+        photo: ['https://static-new.lhw.com/HotelImages/Final/LW1822/lw1822_88438233_720x450.jpg', 'https://y.cdrst.com/foto/hotel-sf/82e8/granderesp/imperial-hotel-tokyo-habitacion-b47a28a.jpg' ,'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/23/d6/20/c7/pool.jpg?w=700&h=-1&s=1'],
+        capacity: 25760,
+        citiId: 'Tokio',
+        userId: '636e884578fa70e8f8c471f5'
+    },
+    {
+        name: 'Hotel Savoy',
+        photo: ['https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/3f/0d/97/piazza-della-repubblica.jpg?w=700&h=-1&s=1', 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/185752001.jpg?k=dce18be2c5d3b46e7b8b3676da6d042d3ba489705af3b994358bc676b5653f25&o=&hp=1','https://cdn.kiwicollection.com/media/property/PR003133/xl/003133-01-HotelSavoy-DuomoPresidentialSuite-Hotel%20Savoy.jpg?cb=1662760732'],
+        capacity: 800,
+        citiId: 'Florencia',
+        userId: '636e884578fa70e8f8c471f6'
+    },
+]
+
+require("dotenv").config()
+require("../../config/database")
+const Hotel = require("../Hotel")
+
+hotels.forEach(e=>{
+    Hotel.create({
+        name: e.name,
+        photo: e.photo,
+        capacity: e.capacity,
+        citiId: e.citiId,
+        userId: e.userId,
+    })
+    
+})
