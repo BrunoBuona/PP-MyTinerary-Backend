ba13a2bafaf3540ee30af07670b3f7737c6b9b95
zincel <106347425+zincel@users.noreply.github.com>
2022-11-11 16:25:31 -0300

    MT-B1-16

---
+++ controllers/user.js
@@ -0,0 +1,21 @@
+const User = require('../models/User')
+
+const controller = {
+
+    create: async (req,res) => {
+        try{
+            let new_user = await User.create(req.body);
+            res.status(201).json({
+                id: new_user._id,
+                success: true,
+                massage: 'the user was successfully created',
+            });
+        }catch(error) {
+            res.status(400).json({
+                success: false,
+                massage: 'error.message',
+            });
+        }
+    },
+};
+module.exports = controller;
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
 let router = require('express').Router()
+let user = require('./users')
+
+router.use('/api/users',user)
 
 module.exports = router;
