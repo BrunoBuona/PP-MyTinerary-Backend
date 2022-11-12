ec58496f2a72c5270d50eab5e375dbece948da21
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-11-12 05:53:31 -0300

    MT-02-23 Backend

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
+require('./config/database')
 var createError = require('http-errors');
 var express = require('express');
+const cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('./config/database')
 
 var indexRouter = require('./routes/index');
 
@@ -14,13 +15,12 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/', indexRouter);
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 let router = require('express').Router()
 let user = require('./users')
+let hotel = require('./hotels')
 
 router.use('/api/users',user)
+router.use('/api/hotels', hotel)
 
 module.exports = router;
