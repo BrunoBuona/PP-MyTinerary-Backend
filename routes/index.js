c2ac1cc20fe5602104a64665898f75d2594d6061
zincel <106347425+zincel@users.noreply.github.com>
2022-11-12 11:16:17 -0300

    MT-01-18

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 let router = require('express').Router()
 let user = require('./users')
+let city = require('./city')
 
 router.use('/api/users',user)
+router.use('/api/cities',city)
 
 module.exports = router;
