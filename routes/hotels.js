55dc20763abb619a9745c60d7f5efb34f1d7ce32
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-11-13 04:07:39 -0300

    MT-B2-30

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,8 +1,9 @@
 var router = require('express').Router()
 
-let {create,update} = require('../controllers/hotel')
+let {create,update,destroy} = require('../controllers/hotel')
 
 router.post('/create',create);
 router.patch('/:id',update);
+router.delete('/:id', destroy);
 
 module.exports = router;
