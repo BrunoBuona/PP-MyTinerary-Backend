edb407a17ba2589fc5837c603aca5d65a5418b9b
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-11-13 04:49:02 -0300

    MT-B2-36

--- controllers/show.js
+++ controllers/show.js
@@ -39,5 +39,28 @@ const controller = {
             });
         }
     },
+    destroy: async function (req, res) {
+        let {id} = req.params;
+        try {
+            let one = await Show.findOneAndDelete({ _id: id});
+            if (one) {
+                res.status(200).json({
+                    id: one._id,
+                    success: true,
+                    message: 'the show was successfully deleted',
+                });
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: 'the show was not found',
+                });
+            }
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    }
 }
 module.exports = controller;
