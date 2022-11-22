7b31adc2532fa009baa51bd5ec983db17ca3d9e2
zincel <106347425+zincel@users.noreply.github.com>
2022-11-22 08:26:49 -0300

    MT-01-41

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ let city = require('./city')
 let shows = require('./show')
 let itinerary = require('./itinerary')
 let myHotels = require('./myhotels')
+let mycities = require('./mycities')
 
 router.use('/api/users',user)
 router.use('/api/hotels', hotel)
@@ -12,5 +13,6 @@ router.use('/api/cities',city)
 router.use('/api/shows',shows)
 router.use('/api/itineraries',itinerary)
 router.use('/api/myhotels', myHotels)
+router.use('/api/mycities', mycities)
 
 module.exports = router;
