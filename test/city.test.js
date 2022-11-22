9796bd8cf13f5cceb6743cd433ee81917fb1e39a
zincel <106347425+zincel@users.noreply.github.com>
2022-11-22 05:17:48 -0300

    MT-01-39

--- test/city.test.js
+++ test/city.test.js
@@ -2,9 +2,11 @@ const app = require("../app");
 const chai = require("chai");
 const assert = chai.assert;
 const request = require("supertest");
+const { expect } = require("chai");
+const { post } = require("../routes");
 
 
-describe('GET /api/cities', function () {
+describe('cities', function () {
 
     it('should return an array of objects', function (done) {
         request(app)
@@ -22,4 +24,31 @@ describe('GET /api/cities', function () {
                 done()
             })
     })
+
+    it('a string was sent in the name field', function (done) {
+        request(app)
+            .post(`/api/cities/`)
+        let user = {
+            name: 'Toronto',
+            continent: 'america',
+            photo: 'https://cms.finnair.com/resource/blob/752416/b5c6b95656c13bf393e5b2c5c87a7cd0/toronto-main-data.jpg?impolicy=crop&width=2943&height=2943&x=529&y=0&imwidth=768',
+            population: 2700000,
+            userId: '636e884578fa70e8f8c471f7'
+        }
+        expect(user.name).to.be.a('string')
+        done()
+    })
+
+    it("404 error when badly creating a hotel", function (done) {
+        request(app)
+            .post("/api/citie/")
+            .expect(404)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                done()
+            })
+    })
 })
+
