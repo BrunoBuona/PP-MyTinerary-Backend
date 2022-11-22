da84327d9cb5e798c2ee15efeabdc421abe3c959
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-11-22 03:25:39 -0300

    MT-02-42

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ let hotel = require('./hotels')
 let city = require('./city')
 let shows = require('./show')
 let itinerary = require('./itinerary')
+let myHotels = require('./myhotels')
 
 router.use('/api/users',user)
 router.use('/api/hotels', hotel)
 router.use('/api/cities',city)
 router.use('/api/shows',shows)
 router.use('/api/itineraries',itinerary)
+router.use('/api/myhotels', myHotels)
 
 module.exports = router;
