3a3f9227f802be970592f99f98c3fd1fb7a71448
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-11-27 15:29:37 -0300

    MT-02-50

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 let router = require('express').Router()
-let {registrar, verificar, signin, loginWithToken,unlogin} = require('../controllers/user')
+let {registrar, verificar, signin, loginWithToken,unlogin, readUserData, updateUserData} = require('../controllers/user')
 const accountExistsSignUp = require('../middlewares/accountExistsSignUp')
 const accountExistsSignIn = require("../middlewares/accountExistsSignIn");
 const mustSignIn = require("../middlewares/mustSignIn.js");
@@ -14,4 +14,6 @@ router.get('/verify/:code',verificar)
 router.post("/signin",validator(schemasignin), accountExistsSignIn, accountHasBeenVerified, signin);
 router.post("/token", passport.authenticate("jwt", { session: false }), mustSignIn, loginWithToken);
 router.put("/signout",passport.authenticate("jwt", { session: false }),unlogin);
+router.get('/me/:id',readUserData)
+router.patch('/me/:id',updateUserData)
 module.exports = router;
