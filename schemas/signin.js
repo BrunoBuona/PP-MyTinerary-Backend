9cfdef4218963ade1948c81f7f75320293f65ee4
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-11-26 21:00:27 -0300

    FIX-MT-02-46

--- schemas/signin.js
+++ schemas/signin.js
@@ -15,7 +15,6 @@ const schema = Joi.object({
     password: Joi
     .string()
     .required()
-    .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
     .messages({
       "string.base": "Please enter a text at name field",
       "any.required": "This field is required, please enter it",
