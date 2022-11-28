b4d24e41e446a397d7ddd79bd932087176040378
zincel <106347425+zincel@users.noreply.github.com>
2022-11-28 07:40:33 -0300

    MT-01-51

--- schemas/itinerary.js
+++ schemas/itinerary.js
@@ -83,11 +83,3 @@ module.exports = schema;
 
 
 
-/* 
-cityId: {type: mongoose.Types.ObjectId, ref: 'cities', required: true},
-name: {type: String, required: true},
-photo: [{type: String, required: true}],
-description: {type: String, required: true},
-price: {type: Number, required: true},
-duration: {type: Number, required: true},
-userId: {type: mongoose.Types.ObjectId, ref: 'users', required: true}, */
