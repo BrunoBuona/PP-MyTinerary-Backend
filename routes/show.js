40dd784eb4ab17a446f7976300ef55ebcc739370
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-11-28 03:48:29 -0300

    MT-02-52

--- routes/show.js
+++ routes/show.js
@@ -1,8 +1,10 @@
 var router = require('express').Router()
+const validator = require('../middlewares/validator')
+const schemashow = require('../schemas/shows')
 
 let {create, update, destroy, readShows} = require('../controllers/show')
 
-router.post('/create',create);
+router.post('/create',validator(schemashow),create);
 router.patch('/:id',update);
 router.delete('/:id',destroy);
 router.get('/', readShows)
