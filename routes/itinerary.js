713756e9bb838ea5a07441c47ff0956ee39572f9
zincel <106347425+zincel@users.noreply.github.com>
2022-11-29 11:25:01 -0300

    FIX-MT-01-52-MT-01-54

--- routes/itinerary.js
+++ routes/itinerary.js
@@ -2,14 +2,14 @@ let router = require('express').Router();
 let { create,update,destroy,readItineraries} = require('../controllers/itinerary');
 const schema = require('../schemas/itinerary')
 const validator = require('../middlewares/validator')
-const passport = require ('../config/passport')
+const passport = require('../config/passport')
 const modelItinerary = require('../models/Itinerary')
 const verifyItinerary = require('../middlewares/verifyItinerary')
 
 
 router.post('/',validator(schema),create);
 router.put('/:id',passport.authenticate("jwt", { session: false }),verifyItinerary(modelItinerary), update)
-router.delete('/:id',passport.authenticate("jwt", { session: false }),verifyItinerary(schema), destroy)
+router.delete('/:id',passport.authenticate("jwt", { session: false }),verifyItinerary(modelItinerary), destroy)
 router.get('/',readItineraries);
 
 module.exports = router;
